3fc0072b331de5e5602d5b38ceeab05af9bdb413
Kanimet <kanimet200127@gmail.com>
2023-05-30 17:14:38 +0600

    Added styles and included images for the layout.

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -1,10 +1,13 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Referral Shop</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap">
     @vite('resources/vue/main.ts')
 </head>
 <body>
--- resources/vue/main.ts
+++ resources/vue/main.ts
@@ -4,6 +4,9 @@ import {createPinia} from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import 'bootstrap'
+import './assets/scss/app.scss'
+
 createApp(App)
     .use(createPinia())
     .use(router)
